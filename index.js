59faa1036b1476379ec22a5fa08574ac7448ec55
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-03-26 02:57:52 +0500

    basic get

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 7000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('TEST !!');
+    res.send('Hello From Home Page .... ');
+});
+
+
+app.listen(PORT, () => console.log(`Server is running on port : http://localhost:${PORT}`));
