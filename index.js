e27f7d3138ce5524d2db8f3b2d501f4463b98684
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-03-27 03:15:23 +0500

    basic node crud

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 7000;
 
 app.use(bodyParser.json());
+
+
+app.use('/users/', userRoutes);
 
 app.get('/', (req, res) => {
     console.log('TEST !!');
